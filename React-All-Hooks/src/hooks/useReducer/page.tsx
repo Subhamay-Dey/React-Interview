31bcdbdf8edae96084833d54693821d20e0f1169
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-10 18:34:10 +0530

    successfully completed my first useReducer problem and changed the previos code to useReducer and yeah useReducer rocked useState shocked :)

--- React-All-Hooks/src/hooks/useReducer/page.tsx
+++ React-All-Hooks/src/hooks/useReducer/page.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state: any, action: { type: any; }) => {
+    switch(action.type) {
+        case "INCREMENT" : 
+            return {count: state.count + 1, text : state.text};
+        
+        case "TextChange" : 
+            return {count: state.count, text : !state.text};
+        
+        default : 
+            return state;
+    }
+};
 
 function UseReducer() {
 
-    const [number, setNumber] = useState(0);
-
-    const [text, setText] = useState(true);
-
-    function click() {
-        setNumber(number + 1);
-        setText(!text)
-    }
+    const [state, dispatch] = useReducer(reducer, {count: 0, text: true});
 
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            {number}
-            <button className="p-2 b-1 bg-black text-white m-2 rounded-md" onClick={click}>Click me</button>
-            {text && "hi"}
+
+            {state.count}
+            
+            <button className="p-2 b-1 bg-black text-white m-2 rounded-md" onClick={() => {
+                dispatch({type:"INCREMENT"});
+                dispatch({type: "TextChange"})    
+            }}>Click me</button>
+            {state.text && "this is a line"}
         </div>
     )
 }
